1f14762b1ea85b0546c28aa94afa197d6ed72889
Alexey Ponomarev <taponomarev@gmail.com>
2021-03-17 20:23:19 +0600

    add phpstan ignore line in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
 use Illuminate\Support\Facades\Route;
 
+/* @phpstan-ignore-next-line */
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
