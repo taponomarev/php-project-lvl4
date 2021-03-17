e636d6c89fe2b4e57bddf461c18d0db3e9459c69
Alexey Ponomarev <taponomarev@gmail.com>
2021-03-17 11:44:49 +0600

    fix i18n

--- resources/lang/ru/validation.php
+++ resources/lang/ru/validation.php
@@ -2,7 +2,7 @@
 
 return [
     'created' => ':name успешно создан:end',
-    'updated' => ':name успешно обновлён:end',
+    'updated' => ':name успешно изменён:end',
     'destroyed' => ':name успешно удалён:end',
     'noRights' => 'У вас нет прав',
     'email' => 'Отправка писем выключена, это тестовый проект'
