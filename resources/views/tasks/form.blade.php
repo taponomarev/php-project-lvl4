16b1a0caa2c7971cf2771ad109401c11af4bd907
Alexey Ponomarev <taponomarev@gmail.com>
2021-03-17 12:40:03 +0600

    fix tasks form

--- resources/views/tasks/form.blade.php
+++ resources/views/tasks/form.blade.php
@@ -9,21 +9,21 @@
 @endif
 <div class="form-group">
     {{ Form::label('name', __('table.name')) }}
-    {{ Form::text('task[name]', $task->name, ['class' => 'form-control']) }}<br>
+    {{ Form::text('name', $task->name, ['class' => 'form-control']) }}<br>
 </div>
 <div class="form-group">
     {{ Form::label('description', __('table.description')) }}
-    {{ Form::text('task[description]', $task->description, ['class' => 'form-control']) }}<br>
+    {{ Form::text('description', $task->description, ['class' => 'form-control']) }}<br>
 </div>
 <div class="form-group">
     {{ Form::label('status_id', __('table.status')) }}
-    {{ Form::select('task[status_id]', $taskStatuses, $task->status->id, ['class' => 'form-control']) }}<br>
+    {{ Form::select('status_id', $taskStatuses, $task->status->id, ['class' => 'form-control']) }}<br>
 </div>
 <div class="form-group">
     {{ Form::label('assigned_to_id', __('table.performer')) }}
-    {{ Form::select('task[assigned_to_id]', $performers, $task->performer->id, ['class' => 'form-control']) }}<br>
+    {{ Form::select('assigned_to_id', $performers, $task->performer->id, ['class' => 'form-control']) }}<br>
 </div>
 <div class="form-group">
     {{ Form::label('labels', __('table.labels'), ['class' => 'text-transform']) }}
-    {{ Form::select('task[labels][]', $labels, $task->labels->pluck('id'), ['class' => 'form-control', 'multiple' => 'multiple']) }}<br>
+    {{ Form::select('labels[]', $labels, $task->labels->pluck('id'), ['class' => 'form-control', 'multiple' => 'multiple']) }}<br>
 </div>
