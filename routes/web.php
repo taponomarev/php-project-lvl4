e1ca567ff0c6b8953219c1bee786504439ec07a2
Alexey Ponomarev <taponomarev@gmail.com>
2021-03-17 20:12:08 +0600

    fix errors for phpstan

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,9 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
 use Illuminate\Support\Facades\Route;
 
-/**
- * @phpstan-ignore-next-line 
-*/
-Route::get(
-    '/', function () {
-        return view('welcome');
-    }
-)->name('welcome');
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
 
 Auth::routes();
 
