8bcafef657a0bff09a2fa1a90ea53003b97f5f2d
Alexey Ponomarev <taponomarev@gmail.com>
2021-03-18 16:12:14 +0600

    add test for labels

--- tests/Feature/LabelControllerTest.php
+++ tests/Feature/LabelControllerTest.php
@@ -2,21 +2,101 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class LabelControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /**
+     * @var bool
+     */
+    protected $seed = true;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        /* @phpstan-ignore-next-line */
+        $user = User::find(1);
+        /** @phpstan-ignore-next-line */
+        $this->actingAs($user);
+    }
+
+    public function testIndex(): void
+    {
+        $response = $this->get('/labels');
+
+        $response->assertOk();
+        $response->assertSee('Метки');
+    }
+
+    public function testCreate(): void
+    {
+        $response = $this->get('/labels/create');
+
+        $response->assertOk();
+        $response->assertSee('Создать метку');
+        $response->assertSeeInOrder([
+            'Имя',
+            'Описание',
+            'Создать'
+        ]);
+    }
+
+    public function testEdit(): void
+    {
+        $response = $this->get('/labels/1/edit');
+
+        $response->assertOk();
+        $response->assertSee('Изменить метку');
+        $response->assertSeeInOrder([
+            'Имя',
+            'Описание',
+            'Обновить'
+        ]);
+    }
+
+    public function testStore(): void
+    {
+        $requestData = [
+            'name' => 'Тестовая метка',
+            'description' => 'Описание метки',
+        ];
+
+        $response = $this->post('/labels', $requestData);
+        $response->assertSessionHasNoErrors();
+        $response->assertSessionHas('flash_notification.0.level', 'success');
+        $response->assertRedirect('/labels');
+        $this->assertDatabaseHas('labels', $requestData);
+    }
+
+    public function testUpdate(): void
+    {
+        $requestData = [
+            'name' => 'Тестовая метка (обновленная)',
+        ];
+
+        $response = $this->patch('/labels/1', $requestData);
+        $response->assertSessionHasNoErrors();
+        $response->assertSessionHas('flash_notification.0.level', 'success');
+        $response->assertRedirect('/labels');
+        $this->assertDatabaseHas('labels', [
+            'id' => 1,
+            'name' => 'Тестовая метка (обновленная)',
+        ]);
+    }
+
+    public function testDestroy(): void
+    {
+        $response = $this->delete('/labels/1');
+        $response->assertSessionHasNoErrors();
+        $response->assertSessionHas('flash_notification.0.level', 'success');
+        $response->assertRedirect('/labels');
+        $this->assertDatabaseMissing('labels', [
+            'id' => 1
+        ]);
     }
 }
